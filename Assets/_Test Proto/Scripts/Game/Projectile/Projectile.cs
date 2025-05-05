8c8bb297d377a7d254a2b7e985f0f93a24f2202e
Andrii <singapurchik666@gmail.com>
2025-05-05 17:43:09 +0300

    little fix

--- Assets/_Test Proto/Scripts/Game/Projectile/Projectile.cs
+++ Assets/_Test Proto/Scripts/Game/Projectile/Projectile.cs
@@ -12,8 +12,6 @@ namespace TestProto.Projectiles
 		[SerializeField] private float _moveSpeed = 15f;
 		[SerializeField] private float _maxDistance = 20f;
 
-		private Coroutine _currentEnableTrailRoutine;
-		
 		private Vector3 _currentDirection;
 
 		private float _timeToStopMove;
@@ -40,29 +38,21 @@ namespace TestProto.Projectiles
 
 		private void StopMove()
 		{
-			if (_currentEnableTrailRoutine != null)
-				StopCoroutine(_currentEnableTrailRoutine);
-			
 			_isMoving = false;
+			_moveTrail.Clear();
 			_moveTrail.enabled = false;
 			OnMoveComplete?.Invoke(this);
 		}
 
 		public void Launch(Vector3 targetPosition, float damage)
 		{
-			_currentEnableTrailRoutine = StartCoroutine(EnableTrailRoutine());
+			_moveTrail.enabled = true;
 			_timeToStopMove = Time.timeSinceLevelLoad + MAX_LIFE_TIME;
 			_body.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
 			_body.LookAt(targetPosition);
 			_currentDirection = (targetPosition - _body.transform.position).normalized;
 			_currentDamage = damage;
 			_isMoving = true;
-		}
-
-		private IEnumerator EnableTrailRoutine()
-		{
-			yield return null;
-			_moveTrail.enabled = true;
 		}
 
 		private void Update()
